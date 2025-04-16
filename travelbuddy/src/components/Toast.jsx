644cfffbe5e6fa0acb28032ef58ce35665fb8d96
Penda <penda25@gwu.edu>
2025-04-16 13:17:03 +0000

    allows going back and forth between pages and added toast notification when adding flight

---
+++ travelbuddy/src/components/Toast.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+function Toast({ message, onClose, duration = 3000 }) {
+  useEffect(() => {
+    const timer = setTimeout(onClose, duration);
+    return () => clearTimeout(timer);
+  }, [onClose, duration]);
+
+  return (
+    <div className="fixed bottom-5 right-5 bg-green-600 text-white px-4 py-2 rounded shadow-lg z-50">
+      {message}
+    </div>
+  );
+}
+
+export default Toast;
+ 
